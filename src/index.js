474d39e194c897cdcdc0382370aac4cf75e8b4a1
serpient <littlegangwolf@gmail.com>
2017-10-05 18:20:31 -0700

    Revert "Revert "Merge branch 'ep/layout-1.0' into NotesFunctionality""
    This reverts commit 594935d1145453a43911d138261e0dc9e191a513.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+<<<<<<< HEAD
 import './index.css';
 import App from './App';
 import Notes from './Notes';
 import Bookmarks from './Bookmarks';
 import RenderToHtml from './Notes';
+=======
+import './styles/index.css';
+import App from './containers/App';
+>>>>>>> ep/layout-1.0
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Notes />, document.getElementById('notes'));
