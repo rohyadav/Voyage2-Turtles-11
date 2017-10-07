3adad625bab22d728cd514ccb759b1f2bbda77ae
serpient <littlegangwolf@gmail.com>
2017-10-07 16:59:56 -0700

    put the icons and tab classes into App.JS so that it can maintain global state for toggleVisibility function. re-worked toggleVisibility.

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -2,89 +2,61 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import '../styles/Notes.css';
 
-const notesArray = [];
-
-class Notes extends Component {
+export class Notes extends Component {
   constructor(props) {
     super(props);
     this.state = {
       quantity: 0,
-      visibility: false,
-      searchInput: '',
       searchTerm: '',
-      titleInput: '',
-      input: '',
-      arrayOfNotes: notesArray,
+      noteTitle: '',
+      noteDescription: '',
+      arrayOfNotes: [],
     }
   }
-  handleSearch = (event) => {
-    this.setState({searchInput: event.target.value});
+  setSearchQuery = (event) => {
+    this.setState({ searchInput: event.target.value });
   }
-  handleSearchSubmit = () => {
-    this.setState({searchTerm: this.state.searchInput});
+  setNoteDescription = (event) => {
+    this.setState({ noteDescription: event.target.value });
   }
-  handleInput = (event) => {
-    this.setState({input: event.target.value});
+  setNoteTitle = (event) => {
+    this.setState({ noteTitle: event.target.value });
   }
-  handleTitleInput = (event) => {
-    this.setState({titleInput: event.target.value})
+  handleNoteSubmit = () => {
+    console.log(this.state.noteDescription);
+    const title = this.state.noteTitle;
+    const value = this.state.noteDescription;
+    this.setState(prevState => ({ arrayOfNotes: prevState.arrayOfNotes.concat([{ 'title': title, 'description': value }]) }));
   }
-  handleSubmit = () => {
-    this.keepCount();
-    const title = this.state.titleInput;
-    const value = this.state.input;
-    const newArray = [...this.state.arrayOfNotes];
-    newArray[newArray.length] = [title, value];
-    this.setState({
-      arrayOfNotes: newArray
-    })
-    notesArray.push(newArray);
-  }
-  toggleVisibility = () =>{
-    this.setState(prevState => ({visibility : !prevState.visibility}));
-    if (this.state.visibility === true) {
-        return ReactDOM.render(<Notes />, document.getElementById('notes'));
-    } else {
-        return ReactDOM.render(<EmptyContainer />, document.getElementById('notes'));
-    }
-  }
-  keepCount = () => {
-    this.setState({quantity: this.state.quantity + 1})
+  closeNotes = () => {
   }
   render() {
-      return (
-        <div>
-          {/* HEADER */}
-          <div className='Notes-Header'>
-            <button className='exitButton' onClick={this.toggleVisibility}>X</button>
-            <h1 className='Notes-Title-Text'>Notes</h1>
-          </div>
-          {/* SEARCH FEATURE */}
-          <div className='Notes-Body'>
-          <textarea searchinput={this.state.searchInput} onChange={this.handleSearch} className='Notes SearchBox SearchBoxText'>
-            Search something!
-          </textarea>
+    return (
+      <div>
+        {/* HEADER */}
+        <div className='Notes-Header'>
+          <button className='exitButton' onClick={this.props.closeHandler}>X</button>
+          <h1 className='Notes-Title-Text'>Notes</h1>
+        </div>
+        {/* SEARCH FEATURE */}
+        <div className='Notes-Body'>
+          <textarea onChange={this.setSearchQuery} className='Notes SearchBox SearchBoxText' defaultValue="Search" />
           <button className='notesButton' onClick={this.handleSearchSubmit}>Search</button>
-          <Search 
-            searchTerm={this.state.searchTerm} array={this.state.arrayOfNotes}/>
+          <Search
+            searchTerm={this.state.searchTerm} notes={this.state.arrayOfNotes} />
 
           {/* NEW NOTE */}
-          <button className='addNotesButton' onClick={this.handleSubmit}>+</button>
-          <textarea className='Notes  NewNoteBoxTitle Title' 
-                    value={this.state.titleInput} 
-                    onChange={this.handleTitleInput}>
-                    Title
-                    </textarea>
-          <textarea className='Notes  NewNoteBoxDescription Description' value={this.state.input} onChange={this.handleInput}>Description</textarea>
-      
-          <h5>Quanity of Notes: {this.state.quantity}</h5>
-          
+          <button className='addNotesButton' onClick={this.handleNoteSubmit}>+</button>
+          <textarea className='Notes NoteTitleTextBox Title' onChange={this.setNoteTitle} defaultValue='Title' />
+          <textarea className='Notes NoteDescriptionTextBox Description' defaultValue='Description' onChange={this.setNoteDescription} />
+          <h5>Quantity of Notes: {this.state.quantity}</h5>
+
           {/* EXISTING NOTE LISTED OUT*/}
-          <RenderNotesAsList 
-            input={this.state.arrayOfNotes}/>
-          </div> 
+          <RenderNotesAsList
+            input={this.state.arrayOfNotes} />
         </div>
-      )
+      </div>
+    )
   }
 }
 
@@ -93,20 +65,18 @@ class RenderNotesAsList extends Component {
     super(props)
     this.mappedElements = [];
   }
-  
+
   render() {
-    this.mappedElements = (this.props.input).map((item,i) =>
+    this.mappedElements = (this.props.input).map((item, i) =>
       <div id={'note_' + i} key={'key_' + i} className='Notes'>
-        <div className="Title">{item[0]}</div>
-        <div className="Description">{item[1]}</div>
+        <div className="Title">{item.title}</div>
+        <div className="Description">{item.description}</div>
       </div>
     );
     return (
       <div>
-        <div> 
-            <h5>Notes:</h5>
-            <div>{this.mappedElements}</div>
-        </div>
+        <h5>Notes:</h5>
+        <div>{this.mappedElements}</div>
       </div>
     )
   }
@@ -115,41 +85,42 @@ class RenderNotesAsList extends Component {
 class Search extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      searchResults: [],
-    }
-    this.resultOfSearchArray = [];
   }
   render() {
-    const array = Array.from(this.props.array);
-    for (var i = 0; i < array.length; i++) {
-      const found = array[i].indexOf(this.props.searchTerm) !== -1;
-      if (found) {
-        this.resultOfSearchArray.push(array[i]);
+    const notes = this.props.notes;
+    const searchTerm = this.props.searchTerm;
+    var formattedSearch = [];
+    if (searchTerm.length) {
+      var searchResults = [];
+      for (var i = 0; i < notes.length; i++) {
+        const found = (notes[i].title.indexOf(searchTerm) !== -1) || (notes[i].description.indexOf(searchTerm) !== -1);
+        if (found) {
+          searchResults.push(notes[i]);
+        }
       }
+      formattedSearch = searchResults.map((item, i) =>
+        <div id={'note_' + i} key={'key_' + i} className='Notes'>
+          <div className="Title">{item.title}</div>
+          <div className="Description">{item.description}</div>
+        </div>
+      );
     }
-    const formattedSearch = this.resultOfSearchArray.map((item,i) =>
-      <div id={'note_' + i} key={'key_' + i} className='Notes'>
-        <div className="Title">{item[0]}</div>
-        <div className="Description">{item[1]}</div>
-      </div>
-    );
+
     return (
-    <div>
       <div>{formattedSearch}</div>
-    </div>
     )
   };
 }
-class EmptyContainer extends React.Component {
+
+export class EmptyContainer extends React.Component {
   constructor(props) {
-      super(props);
+    super(props);
   }
   render() {
-      return (
-          <div></div>
-      )
+    console.log("render");
+    return (
+      <div></div>
+    )
   }
 }
-export default Notes;
 
