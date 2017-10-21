91a197147ff1f60e7f8936337fa9e1d1e437a64a
serpient <littlegangwolf@gmail.com>
2017-10-20 20:19:53 -0700

    updated more icons and gmail/github links

--- src/containers/App.js
+++ src/containers/App.js
@@ -118,15 +118,57 @@ class TodosButton extends React.Component {
 
 
 class GmailButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      iconLink: './assets/mail.png',
+    };
+  }
+  iconChangeOnHover = () => {
+    this.setState({ iconLink: './assets/mail_hover.png' });
+  }
+  iconChangeOnOut = () => {
+    this.setState({ iconLink: './assets/mail.png' });
+  }
   render() {
     return (
       <div className="item">
-        <a href="https://accounts.google.com/signin/v2/sl/pwd?service=mail&passive=true&rm=false&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F%3Ftab%3Dwm&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin">
-          <img src={require('../assets/mail.png')} alt="gmail" />
+        <a href="https://accounts.google.com/signin/v2/sl/pwd?service=mail&passive=true&rm=false&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F%3Ftab%3Dwm&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin"
+          onMouseOver={this.iconChangeOnHover}
+          onMouseOut={this.iconChangeOnOut} >
+          <img src={this.state.iconLink} alt="gmail" />
         </a>
         <p>Gmail</p>
-      </div>);
+      </div>
+    );
+  } 
+}
+
+class GithubButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      iconLink: './assets/Icons_COLOR_backgrounds_github.png',
+    };
   }
+  iconChangeOnHover = () => {
+    this.setState({ iconLink: './assets/Icons_COLOR_backgrounds_github_hover.png' });
+  }
+  iconChangeOnOut = () => {
+    this.setState({ iconLink: './assets/Icons_COLOR_backgrounds_github.png' });
+  }
+  render() {
+    return (
+      <div className="item">
+        <a href="https://github.com/"
+          onMouseOver={this.iconChangeOnHover}
+          onMouseOut={this.iconChangeOnOut} >
+          <img src={this.state.iconLink} alt="github" />
+        </a>
+        <p>Github</p>
+      </div>
+    );
+  } 
 }
 
 function tab_open() {
@@ -202,10 +244,7 @@ class App extends Component {
                   <p>History</p>
                 </div>
                 <GmailButton />
-                <div className="item">
-                  <img src="assets/Icons_COLOR_background-01.png" alt="Placeholder" />
-                  <p>Placeholder</p>
-                </div>
+                <GithubButton />
               </div>
             </div>
           </div>
