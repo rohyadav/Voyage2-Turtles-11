f63b071c73dc1935943e252aa9769f6878347be6
Eunice Park <28845173+eunipa@users.noreply.github.com>
2017-11-13 15:39:26 -0800

    Gmail, GitHub links open in new tab

--- src/containers/App.js
+++ src/containers/App.js
@@ -140,7 +140,8 @@ class GmailButton extends React.Component {
       <div className="item">
         <a href="https://accounts.google.com/signin/v2/sl/pwd?service=mail&passive=true&rm=false&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F%3Ftab%3Dwm&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin"
           onMouseOver={this.iconChangeOnHover}
-          onMouseOut={this.iconChangeOnOut} >
+          onMouseOut={this.iconChangeOnOut} 
+          target="_blank" >
           <img src={this.state.iconLink} alt="gmail" />
         </a>
         <p>Gmail</p>
@@ -167,7 +168,8 @@ class GithubButton extends React.Component {
       <div className="item">
         <a href="https://github.com/"
           onMouseOver={this.iconChangeOnHover}
-          onMouseOut={this.iconChangeOnOut} >
+          onMouseOut={this.iconChangeOnOut} 
+          target="_blank" >
           <img src={this.state.iconLink} alt="github" />
         </a>
         <p>Github</p>
