53dca17da9cb96993a4f756dff7d690d73a6623e
Eunice Park <28845173+eunipa@users.noreply.github.com>
2017-11-13 16:37:35 -0800

    Save selected background image in localStorage

--- src/containers/App.js
+++ src/containers/App.js
@@ -293,6 +293,16 @@ const Time = () => {
   )
 }
 
+/* --------- Background Image LocalStorage --------- */
+
+let bgImgVariable;
+
+if(!localStorage.getItem('bgImgStored')) {
+  bgImgVariable = bg1;
+} else {
+  bgImgVariable = localStorage.getItem('bgImgStored')
+}
+
 /* =========================
  MAIN APP COMPONENT - RENDERS ENTIRE PAGE
  =========================== */
@@ -306,7 +316,7 @@ const Time = () => {
       historyTabOpen: "false",
       weatherTabOpen: "false",
       appsTabOpen: "false",
-      image: bg1
+      image: bgImgVariable
     };
   }
 
@@ -488,12 +498,14 @@ const Time = () => {
 
   /* ---- Background Image ---- */
   backgroundChange = () => {
-    let bgImage = this.state.image;
+    // let bgImage = this.state.image;
     const bgArray = [bg1, bg2, bg3, bg4, bg5, bg6, bg7, bg8, bg9,
                     bg10, bg11, bg12, bg13, bg14, bg15, bg16, bg17];
     let randomNumber = Math.floor(Math.random() * (bgArray.length));
-    bgImage = bgArray[randomNumber];
-    this.setState({image: bgImage});
+    let bgImage = bgArray[randomNumber];
+    this.setState( {image: bgImage}, () => localStorage.setItem('bgImgStored', this.state.image) );
+    // this.setState({image: bgImage});
+    // localStorage.setItem('bgImgStored', this.state.image);  
   }
 
   render() {
