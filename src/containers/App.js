25596713a0fb4881f3e8c98a573f9eaf8ee6edef
serpient <littlegangwolf@gmail.com>
2017-11-14 22:47:02 -0800

    app.js - close exit button works for tabs

--- src/containers/App.js
+++ src/containers/App.js
@@ -109,7 +109,12 @@ class TodosButton extends React.Component {
   }
 
   render() {
-    let todoArrLenInit = JSON.parse(localStorage["todoData-todoArr"]).length;
+    let todoArrLenInit;
+    if (localStorage["todoData-todoArr"] === undefined || localStorage["todoData-todoArr"] === null) {
+      todoArrLenInit = 0;
+    } else {
+      todoArrLenInit = JSON.parse(localStorage["todoData-todoArr"]).length
+    };
     return (<div className="item">
       <a
         onClick={this.props.clickHandler}
@@ -141,8 +146,8 @@ class GmailButton extends React.Component {
       <div className="item">
         <a href="https://accounts.google.com/signin/v2/sl/pwd?service=mail&passive=true&rm=false&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F%3Ftab%3Dwm&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin"
           onMouseOver={this.iconChangeOnHover}
-          onMouseOut={this.iconChangeOnOut} 
-          target="_blank" 
+          onMouseOut={this.iconChangeOnOut}
+          target="_blank"
           rel="noopener noreferrer">
           <img src={this.state.iconLink} alt="gmail" />
         </a>
@@ -170,8 +175,8 @@ class GithubButton extends React.Component {
       <div className="item">
         <a href="https://github.com/"
           onMouseOver={this.iconChangeOnHover}
-          onMouseOut={this.iconChangeOnOut} 
-          target="_blank" 
+          onMouseOut={this.iconChangeOnOut}
+          target="_blank"
           rel="noopener noreferrer">
           <img src={this.state.iconLink} alt="github" />
         </a>
@@ -326,7 +331,7 @@ class Time extends React.Component {
 
 let bgImgVariable;
 
-if(!localStorage.getItem('bgImgStored')) {
+if (!localStorage.getItem('bgImgStored')) {
   bgImgVariable = bg1;
 } else {
   bgImgVariable = localStorage.getItem('bgImgStored')
@@ -335,192 +340,90 @@ if(!localStorage.getItem('bgImgStored')) {
 /* =========================
  MAIN APP COMPONENT - RENDERS ENTIRE PAGE
  =========================== */
- class App extends Component {
+const TabType = {
+  TODO: 0,
+  NOTES: 1,
+  BOOKMARKS: 2,
+  HISTORY: 3,
+  WEATHER: 4,
+  APPS: 5,
+  COUNT: 6, // Update this as tab type increases.
+}
+
+const TABS = [
+  { divName: "todo", domElem: <TodoList closeHandler={AppToggleVisibility} /> },
+  { divName: "notes", domElem: <Notes closeHandler={AppToggleVisibility} /> },
+  { divName: "bookmarks", domElem: <Bookmarks closeHandler={AppToggleVisibility} /> },
+  { divName: "history", domElem: <History closeHandler={AppToggleVisibility} /> },
+  { divName: "weather", domElem: <Weather closeHandler={AppToggleVisibility} /> },
+  { divName: "apps", domElem: <AppsTab closeHandler={AppToggleVisibility} /> },
+];
+
+let APP_COMPONENT_INSTANCE = {};
+function AppToggleVisibility(param, event)
+{
+  APP_COMPONENT_INSTANCE.toogleVisibility(param, event);
+}
+
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      tabStatus: [
-        {
-          todoTabOpen: "false",
-          notesTabOpen: "false",
-          bookmarksTabOpen: "false",
-          historyTabOpen: "false",
-          weatherTabOpen: "false",
-          appsTabOpen: "false",
-        }
-      ],
+      tabIsOpen: [],
       image: bgImgVariable,
       time: ""
     };
-  }
 
-  allTabsClosed = () => {
-
-    this.setState({
-      tabStatus: [{
-          todoTabOpen: "false",
-          notesTabOpen: "false",
-          bookmarksTabOpen: "false",
-          historyTabOpen: "false",
-          weatherTabOpen: "false",
-          appsTabOpen: "false",
-      }]
-    });
-
-    ReactDOM.render(<EmptyContainer />, document.getElementById('todo'));
-    ReactDOM.render(<EmptyContainer />, document.getElementById('notes'));
-    ReactDOM.render(<EmptyContainer />, document.getElementById('bookmarks'));
-    ReactDOM.render(<EmptyContainer />, document.getElementById('history'));
-    ReactDOM.render(<EmptyContainer />, document.getElementById('weather'));
-    ReactDOM.render(<EmptyContainer />, document.getElementById('apps'));
-    tab_close();
+    APP_COMPONENT_INSTANCE = this;
+    for (var i = 0; i < TabType.COUNT; ++i) {
+      this.state.tabIsOpen.push(false);
+    }
   }
 
   toogleVisibility = (param, event) => {
+    let newOpenTabs = [];
+    for (var i = 0; i < TabType.COUNT; ++i) {
+      newOpenTabs.push(false);
+    }
+
     switch (param) {
       // todo icon pressed
       case "todo":
-        switch (this.state.tabStatus[0].todoTabOpen) {
-          case "true":
-            this.allTabsClosed();
-            break;
-          case "false":
-            this.setState({
-              tabStatus: [{...this.state.tabStatus, todoTabOpen: "true"}]
-            });
-            ReactDOM.render(<TodoList closeHandler={this.toogleVisibility} />, document.getElementById('todo'));
-            ReactDOM.render(<EmptyContainer />, document.getElementById('notes'));
-            ReactDOM.render(<EmptyContainer />, document.getElementById('bookmarks'));
-            ReactDOM.render(<EmptyContainer />, document.getElementById('history'));
-            ReactDOM.render(<EmptyContainer />, document.getElementById('weather'));
-            ReactDOM.render(<EmptyContainer />, document.getElementById('apps'));
-            tab_open();
-            break;
-          default:
-            break;
-        }
+        newOpenTabs[TabType.TODO] = !this.state.tabIsOpen[TabType.TODO];
         break;
       // notes icon pressed
       case "notes":
-        switch (this.state.tabStatus[0].notesTabOpen) {
-          case "true":
-            this.allTabsClosed();
-            break;
-          case "false":
-            this.setState({
-              tabStatus: [{...this.state.tabStatus, notesTabOpen: "true"}]
-            });
-            ReactDOM.render(<EmptyContainer />, document.getElementById('todo'));
-            ReactDOM.render(<Notes closeHandler={this.toogleVisibility} />, document.getElementById('notes'));
-            ReactDOM.render(<EmptyContainer />, document.getElementById('bookmarks'));
-            ReactDOM.render(<EmptyContainer />, document.getElementById('history'));
-            ReactDOM.render(<EmptyContainer />, document.getElementById('weather'));
-            ReactDOM.render(<EmptyContainer />, document.getElementById('apps'));
-            tab_open();
-            break;
-          default:
-            break;
-        }
+        newOpenTabs[TabType.NOTES] = !this.state.tabIsOpen[TabType.NOTES];
         break;
       // bookmarks icon pressed
       case "bookmarks":
-        switch (this.state.tabStatus[0].bookmarksTabOpen) {
-          case "true":
-            this.allTabsClosed();
-            break;
-          case "false":
-            this.setState({
-              tabStatus: [{...this.state.tabStatus, bookmarksTabOpen: "true"}]
-            });
-            ReactDOM.render(<EmptyContainer />, document.getElementById('todo'));
-            ReactDOM.render(<EmptyContainer />, document.getElementById('notes'));
-            ReactDOM.render(<Bookmarks closeHandler={this.toogleVisibility} />, document.getElementById('bookmarks'));
-            ReactDOM.render(<EmptyContainer />, document.getElementById('history'));
-            ReactDOM.render(<EmptyContainer />, document.getElementById('weather'));
-            ReactDOM.render(<EmptyContainer />, document.getElementById('apps'));
-            tab_open();
-            break;
-          default:
-            break;
-        }
+        newOpenTabs[TabType.BOOKMARKS] = !this.state.tabIsOpen[TabType.BOOKMARKS];
         break;
       // history icon pressed
       case "history":
-        switch (this.state.tabStatus[0].historyTabOpen) {
-          case "true":
-            this.allTabsClosed();
-            break;
-          case "false":
-            this.setState({
-              tabStatus: [{...this.state.tabStatus, historyTabOpen: "true"}]
-            });
-            ReactDOM.render(<EmptyContainer />, document.getElementById('todo'));
-            ReactDOM.render(<EmptyContainer />, document.getElementById('notes'));
-            ReactDOM.render(<EmptyContainer />, document.getElementById('bookmarks'));
-            ReactDOM.render(<History closeHandler={this.toogleVisibility} />, document.getElementById('history'));
-            ReactDOM.render(<EmptyContainer />, document.getElementById('weather'));
-            ReactDOM.render(<EmptyContainer />, document.getElementById('apps'));
-            tab_open();
-            break;
-          default:
-            break;
-        }
-        break;  
-        case "weather":
-        switch (this.state.tabStatus[0].weatherTabOpen) {
-          case "true":
-            this.allTabsClosed();
-            break;
-          case "false":
-            this.setState({
-              tabStatus: [{...this.state.tabStatus, weatherTabOpen: "true"}]
-            });
-            ReactDOM.render(<EmptyContainer />, document.getElementById('todo'));
-            ReactDOM.render(<EmptyContainer />, document.getElementById('notes'));
-            ReactDOM.render(<EmptyContainer />, document.getElementById('bookmarks'));
-            ReactDOM.render(<EmptyContainer />, document.getElementById('history'));
-            ReactDOM.render(<Weather closeHandler={this.toogleVisibility} />, document.getElementById('weather'));
-            ReactDOM.render(<EmptyContainer />, document.getElementById('apps'));
-            tab_open();
-            break;
-          default:
-            break;
-        }
-        break;  
-        case "apps":
-        switch (this.state.tabStatus[0].appsTabOpen) {
-          case "true":
-            this.allTabsClosed();
-            break;
-          case "false":
-            this.setState({
-              tabStatus: [{...this.state.tabStatus, appsTabOpen: "true"}]
-            });
-            ReactDOM.render(<EmptyContainer />, document.getElementById('todo'));
-            ReactDOM.render(<EmptyContainer />, document.getElementById('notes'));
-            ReactDOM.render(<EmptyContainer />, document.getElementById('bookmarks'));
-            ReactDOM.render(<EmptyContainer />, document.getElementById('history'));
-            ReactDOM.render(<EmptyContainer />, document.getElementById('weather'));
-            ReactDOM.render(<AppsTab closeHandler={this.toogleVisibility} />, document.getElementById('apps'));
-            tab_open();
-            break;
-          default:
-            break;
-        }
-        break;  
+        newOpenTabs[TabType.HISTORY] = !this.state.tabIsOpen[TabType.HISTORY];
+        break;
+      case "weather":
+        newOpenTabs[TabType.WEATHER] = !this.state.tabIsOpen[TabType.WEATHER];
+        break;
+      case "apps":
+        newOpenTabs[TabType.APPS] = !this.state.tabIsOpen[TabType.APPS];
+        break;
       // the exit button from one of the open tabs have been pressed
       default:
-        this.allTabsClosed();
+        //this.allTabsClosed();
     }
+
+    this.setState({tabIsOpen : newOpenTabs});
   }
 
   /* ---- Background Image ---- */
   backgroundChange = () => {
     const bgArray = [bg1, bg2, bg3, bg4, bg5, bg6, bg7, bg8, bg9,
-                    bg10, bg11, bg12, bg13, bg14, bg15, bg16, bg17];
+      bg10, bg11, bg12, bg13, bg14, bg15, bg16, bg17];
     let randomNumber = Math.floor(Math.random() * (bgArray.length));
     let bgImage = bgArray[randomNumber];
-    this.setState( {image: bgImage}, () => localStorage.setItem('bgImgStored', this.state.image) ); // Works with callback
+    this.setState({ image: bgImage }, () => localStorage.setItem('bgImgStored', this.state.image)); // Works with callback
   }
 
   render() {
@@ -533,12 +436,30 @@ if(!localStorage.getItem('bgImgStored')) {
       minHeight: '100vh',
     }
 
+    // For all the tabs, determine their visibility and inject the correct DOM elements into the page.
+    let tabPanes = [];
+    let anyTabsOpen = false;
+    for (var i = 0; i < TabType.COUNT; ++i) {
+      if (this.state.tabIsOpen[i]) {
+        tabPanes.push(<div id={TABS[i].divName}>{TABS[i].domElem}</div>);
+        anyTabsOpen = true;
+      }
+      else {
+        tabPanes.push(<div id={TABS[i].divName}><EmptyContainer/></div>);
+      }
+    }
+
+    let mainStyle = {marginRight : "0px"};
+    if (anyTabsOpen) {
+      mainStyle = {marginRight : "300px"};
+    }
+
     return (
       <div className="App" style={bgStyle} >
-        <div className="main" id="main">
+        <div className="main" id="main" style={mainStyle}>
           <div className="main-content">
             <div className="main-top" >
-                <Time />
+              <Time />
               <div className="search-area">
                 <GoogleSearch
                   types={
@@ -584,7 +505,7 @@ if(!localStorage.getItem('bgImgStored')) {
               className="footer-icon"
               src={rndomImgIcon}
               alt="Turtles Cohort"
-              onClick={this.backgroundChange} 
+              onClick={this.backgroundChange}
               title="Click to change background" />
             <p className="leftFooter">Photos by Natasha Sadikin</p>
             {/*<div className="footer-margin"></div>*/}
@@ -592,20 +513,7 @@ if(!localStorage.getItem('bgImgStored')) {
         </div> {/* .main */}{/* controls what part of main will shift when tab opens */}
 
         <div id='tabs'>
-          <aside>
-            <div id="weather">
-            </div>
-            <div id="todo">
-            </div>
-            <div id="apps">
-            </div>
-            <div id="notes" >
-            </div>
-            <div id="bookmarks">
-            </div>
-            <div id="history">
-            </div>
-          </aside>
+          {tabPanes}
         </div>
 
       </div> // end of .App container
